794f61772d543b0e5beae046e736a0cea4567b26
ChengLiufeng <liufeng.cheng@ericsson.com>
2014-10-29 23:53:07 +0800

    listview pull to refresh data and auto load finished

--- src/com/geekcoder/fragment/TestFragment.java
+++ src/com/geekcoder/fragment/TestFragment.java
@@ -5,47 +5,62 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import com.example.geekcoder.R;
 import com.geekcoder.activity.NewsDetailActivity;
+import com.geekcoder.adapter.ListViewAdapter;
+import com.geekcoder.widget.AutoListView;
+import com.geekcoder.widget.AutoListView.OnLoadListener;
+import com.geekcoder.widget.AutoListView.OnRefreshListener;
 
-public class TestFragment extends Fragment {
-
+public class TestFragment extends Fragment implements OnRefreshListener,OnLoadListener {
+	
+	private AutoListView lstv;
+	private ListViewAdapter adapter;
+	private List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+	private Handler handler = new Handler() {
+		public void handleMessage(Message msg) {
+			List<Map<String, Object>> result = (List<Map<String, Object>>) msg.obj;
+			switch (msg.what) {
+			case AutoListView.REFRESH:
+				lstv.onRefreshComplete();
+				list.clear();
+				list.addAll(result);
+				break;
+			case AutoListView.LOAD:
+				lstv.onLoadComplete();
+				list.addAll(result);
+				break;
+			}
+			lstv.setResultSize(result.size());
+			adapter.notifyDataSetChanged();
+		};
+	};
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-    	View rootView = inflater.inflate(R.layout.guide_2, container, false);
-        ListView listview = (ListView) rootView.findViewById(R.id.lv);
-        List<Map<String,Object>> listitems=new ArrayList<Map<String,Object>>();
-         for (int i = 0; i < 50; i++) {
-            Map<String,Object> map = new HashMap<String,Object>();
-            map.put("image",R.drawable.test);
-            map.put("title", "中国未上空气最差20城排行榜");
-            map.put("description", "20个城市中印度占一半；兰州最高36位，北京排76位。");
-            listitems.add(map);
-        }
-          
-         SimpleAdapter simpleAdapter=new SimpleAdapter(
-                 getActivity(),
-                 listitems,
-                 R.layout.item,
-                 new String[]{"image","title","description"}, 
-                 new int[]{R.id.image, R.id.title,R.id.description});
-         listview.setAdapter(simpleAdapter);
-         //对于listview中的每一个列表项，设置单击的事件监听器
-         listview.setOnItemClickListener(new OnItemClickListener(){
-
+    	View rootView = inflater.inflate(R.layout.fragment, container, false);
+		lstv = (AutoListView) rootView.findViewById(R.id.lstv);
+		adapter = new ListViewAdapter(getActivity(), list);
+		lstv.setAdapter(adapter);
+		lstv.setOnRefreshListener(this);
+		lstv.setOnLoadListener(this);
+		initData();
+        //对于listview中的每一个列表项，设置单击的事件监听器
+        lstv.setOnItemClickListener(new OnItemClickListener(){
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
@@ -57,7 +72,55 @@ public class TestFragment extends Fragment {
 				intent.setClass(getActivity().getApplicationContext(), NewsDetailActivity.class);
 				startActivity(intent);
 			}
-         });
+        });
          return rootView;
     }
+    
+	private void initData() {
+		loadData(AutoListView.REFRESH);
+	}
+
+	private void loadData(final int what) {
+		// 杩欓噷妯℃嫙浠庢湇鍔″櫒鑾峰彇鏁版嵁
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(700);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				Message msg = handler.obtainMessage();
+				msg.what = what;
+				msg.obj = getData();
+				handler.sendMessage(msg);
+			}
+		}).start();
+	}
+
+	@Override
+	public void onRefresh() {
+		loadData(AutoListView.REFRESH);
+	}
+
+	@Override
+	public void onLoad() {
+		loadData(AutoListView.LOAD);
+	}
+
+	// 获取测试的数据
+	public List<Map<String, Object>> getData() {
+		List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
+		Random random = new Random();
+		for (int i = 0; i < 10; i++) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			long l = random.nextInt(10000);
+			map.put("thumbImage", R.drawable.test);
+			map.put("newsTitle", "测试的新闻标题" + l);
+			map.put("newsSummary", "测试新闻摘要" + l);
+			result.add(map);
+		}
+		return result;
+	}
 }
